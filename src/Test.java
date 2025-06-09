09dedf89e946633500f2424b3dce95cca2d997be
ram <sairampadala001@gmail.com>
2025-06-09 23:32:11 +0530

    snack chnaged some code

--- src/Test.java
+++ src/Test.java
@@ -8,6 +8,10 @@ public class Test {
       if(country.equalsIgnoreCase("IN")){
         System.out.println(locale.getLanguage());
         System.out.println("code is not working fine");
+        System.out.println("still working fine");
+        System.out.println("yes its working");
+        System.out.println("plese check the code once");
+        System.out.println("code check done");
         
       }
     }
